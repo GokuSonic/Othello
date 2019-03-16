b5dbfd20187d3d2f1e8486bebfc58a05c878062a
caldw026 <caldw026@empress.csusm.edu>
2019-03-15 20:17:57 -0700

    First commit to everyone

---
+++ othello.cpp
@@ -0,0 +1,23 @@
+
+#include <iostream>
+#include "board.h"
+
+
+//start of the game
+int main()
+{
+
+    //creates a new instance of the game
+    board myGame;
+
+    cout <<"\n----Player's turn----" <<endl;
+    myGame.drawBoard();
+    cout << "Choose one of the moves: " << endl;
+
+
+
+    return 0;
+}
+
+
+
